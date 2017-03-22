f7fac8c33cb914c9082403d1a91d6ce7d2ddef08
Yeah-Kun <767786685@qq.com>
2017-03-22 23:23:01 +0800

    finish my double_linked_list

--- C/Double_linked_list.c
+++ C/Double_linked_list.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define int elemtype
+typedef int elemtype;
 typedef struct Node
 {
 	elemtype data;
@@ -11,34 +11,65 @@ typedef struct Node
 
 LinkedList LinkedListCreate(elemtype x)
 {
-	int i = 0;   //count the number 
+	int i = 1;   //count the number 
 	int keep;    // to save the number
-	Node head,p,q;
+	Node *head,*p,*q;
 	head = (Node*) malloc (sizeof(Node));
 	if(head == NULL)                       
 		printf("Failed to apply memory space!\n");
-	head->next  = NULL; 
-	head->prior = NULL;
+	head->next  = head; 
+	head->prior = head;
 	if(x > 0)          //do not raise any memory space
 	{
 		scanf("%d",&keep);
 		head->data = keep;
+		q = head;
 	}
 	for(;i<x;i++)
 	{
 		p = (Node*) malloc (sizeof(Node));
 		scanf("%d",&keep);
 		p->data = keep;
-		head->next  = p;
 		head->prior = p;
 		p->next  = head;
-		p->prior = head;
-		
+		q->next  = p;
+		p->prior = q;
+		q = p;
 	}
+	return head;
+}
+
+LinkedList LinkedListSearch(LinkedList head,elemtype x)
+{
+	Node *p;
+	if(head->data == x)
+		return head;
+	p = head->next;
+	
+	while(p != head)
+	{
+		if(p->data == x)
+			return p;
+		p = p->next;
+	}
+	return 0;
 }
 
 int main()
 {
-	
+	Node *head;
+	int flag;
+	int x;
+	printf("Please enter a len number:\n");
+	scanf("%d",&x);
+	head = LinkedListCreate(x);
+	printf("Please enter the number what you fined:\n");
+	scanf("%d",&x);
+	flag = LinkedListSearch(head,x);
+	if(flag == 0)
+		printf("Can't found the value.\n");
+	else
+		printf("The value in %d\n",flag);
 	return 0;
 }
+
