413aa47baa53a8e2b1088a004c7c7ddd335b600d
Yeah-Kun <767786685@qq.com>
2017-03-22 23:24:24 +0800

    beginning code

---
+++ C/PolishNotation_Stack.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define N 100
+typedef int elemtype;
+
+//define the Stack
+typedef struct SequenStack
+{
+	elemtype data[];
+	elemtype *top;
+}Stack;
+
+// create a 
+Stack StackCreate()
+{
+
+}
+
+int main()
+{
+
+}
